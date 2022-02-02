bc28287c122c590452235f9b0baab87fbd538157
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-01 22:27:57 -0500

    Add User Model to db

---
+++ backend/db/migrations/20220202032210-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      unique: true
+    },
+    hashedPassword: {
+      type: Sequelize.STRING.BINARY,
+      allowNull: false,
+    },
+    bio: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    isHost: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('now'),
+    },
+    updatedAt: {
+      allowNull: true,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('now'),
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+};
