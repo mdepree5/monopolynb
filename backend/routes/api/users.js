ace17f600ccd651c9dd7a2fdeff3391601b494ee
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-02 00:38:18 -0500

    Comment out 'hello/world' api route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -66,20 +66,40 @@ router.route('/')
 )
 
 
-// fetch('/api/users', {
-//   method: 'POST',
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `Lh2ObvS1-UWftESa5wxo3N4yDw-BTvDKN6AM`
-//   },
-//   body: JSON.stringify({
-//     email: 'cheaters@neverprosper.com',
-//     username: 'yourCousin',
-//     password: 'loadedDie',
-      
-//   })
-// }).then(res => res.json()).then(data => console.log(data));
+// ! —————————————————————————————————————————————————————————————————————————————————————
+//! TEST SIGNUP VALIDATIONS IN BROWSER WITH DEV TOOLS
+/* 
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `Lh2ObvS1-UWftESa5wxo3N4yDw-BTvDKN6AM`
+  },
+  body: JSON.stringify({
+    email: 'firestar@spider.man',
+    username: 'Firestar',
+    password: ''
+  })
+}).then(res => res.json()).then(data => console.log(data));
+ */
 
+/* 
+todo —————————————————————————————————————————————————————————————————————————————————————
+todo HERE
 
+Try to sign up with more invalid fields to test out the checks in the validateSignup middleware. 
+Make sure to cover each of the following test cases which should give back a Bad Request error:
+
+email field is an empty string
+email field is not an email
+username field is an empty string
+username field is only 3 characters long
+username field is an email
+password field is only 5 characters long
+If you don't see the Bad Request error for any of these, check your syntax for the validateSignup middleware.
+
+Commit your code once you're done testing!
+*/
+// ! —————————————————————————————————————————————————————————————————————————————————————
 
 module.exports = router;
