a21f745c67bbc83f30c28561807593708cd58e95
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-02 00:33:46 -0500

    Add custom express-validator validators to register (post) route in users api

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,34 +10,44 @@ const { User } = require('../../db/models');
 const router = express.Router();
 
 
+// ! —————————————————————————————————————————————————————————————————————————————————————
+// ! Elaborate express-validator validators!!!
 const validateSignup = [
   check('username')
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
+    .withMessage('Please provide a username with at least 4 characters.')
+    .isLength({ max: 25 })
+    .withMessage('Please provide a username with no more than 25 characters.')
     .not()
     .isEmail()
     .withMessage('Username cannot be an email.'),
   check('firstName')
     .exists({ checkFalsy: true })
     .isLength({ min: 1 })
-    .withMessage('Please provide a first name with at least 1 character.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
+    .withMessage('Please provide a first name with at least 1 character.')
+    .isLength({ max: 25 })
+    .withMessage('Please provide a first name with no more than 25 characters.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .withMessage('Please provide a last name with at least 1 character.')
+    .isLength({ max: 25 })
+    .withMessage('Please provide a last name with no more than 25 characters.'),
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Please provide a valid email.')
+    .custom(value => User.findByEmail(value).then(user => {
+      if(user) return Promise.reject('Email already in use')
+    })),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
   handleValidationErrors
 ];
-
+// ! —————————————————————————————————————————————————————————————————————————————————————
 
 
 router.route('/')
