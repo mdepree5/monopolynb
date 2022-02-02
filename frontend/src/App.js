efb26fadfde085a4c8d9c77e3d6e9e6ac0a68681
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-02 16:44:42 -0500

    Add dropdown to ProfileButton in Navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import Navigation from "./components/Navigation";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,17 +13,19 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
+  return (
     <>
-    <Navigation />
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup">
-        <SignupFormPage />
-      </Route>
-    </Switch>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
     </>
   );
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,12 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
 
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
-const Navigation = () => (
-  <div>
-    Hello
-    <ProfileButton />
-  </div>
-);
+  let sessionLinks;
+
+  sessionLinks = sessionUser ? (
+    <ProfileButton user={sessionUser} />
+  ) : (
+    <>
+      <NavLink to="/login">Log In</NavLink>
+      <NavLink to="/signup">Sign Up</NavLink>
+    </>
+  );
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
+}
 
 export default Navigation;
