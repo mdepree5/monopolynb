1fa9129f61b2dfce9260f5dfb7a181f8cb026458
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-01 23:38:41 -0500

    Correct User Method scopes in User Model, Correct token-cookie test API route in api/index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
 
+// todo ——————————————————————————————————————————————————————————————————————————————————
+//todo Testing API routes
 
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'demoUser1'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// POST /api/test
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
+
+
+// todo ——————————————————————————————————————————————————————————————————————————————————
+
 
 
 
