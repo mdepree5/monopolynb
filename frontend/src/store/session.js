73aceaccf33ee2456a47fcf1f1d1e335dc334af7
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-02 16:18:20 -0500

    Add session user logout thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -59,6 +59,16 @@ export const signup = user => async (dispatch) => {
 };
 
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+
+
 
 
 const initialState = { user: null };
