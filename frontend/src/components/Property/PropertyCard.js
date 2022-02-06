2eb3e7cfacb7019cf332dbe9c2a5f5753b0f93f7
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-06 16:52:19 -0500

    Refactor PropertyCard

--- frontend/src/components/Property/PropertyCard.js
+++ frontend/src/components/Property/PropertyCard.js
@@ -7,7 +7,7 @@ const PropertyCard = ({ property }) => (
     <div className='property-host'> 
       <Link to={`/users/${property.hostId}`}/>
     </div>
-    <div className='property-title'>{`Title: ${property.title}`}</div>
+    <h4 className='property-title'>{property.title}</h4>
     <div className='property-description'>{`Description: ${property.description}`}</div>
     <div className='property-number-of-beds'>{`Number of Beds: ${property.numberOfBeds}`}</div>
     <div className='property-price'>{`Price: ${property.price}`}</div>
