810c6fc2edbd371ca2a84e179e6388a7533ae167
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-08 19:56:56 -0500

    Style footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,7 +2,10 @@ import './Footer.css';
 
 const Footer = () => (
   <div className='footer'>
-    <h2>Footer</h2>
+    <ul className='footer-links'>
+      <li><a href='https://github.com/mdepree5'>Github</a></li>
+      <li><a href='https://www.appacademy.io/'>App Academy</a></li>
+    </ul>
   </div>
 )
 
