02e7df7598a85ec04f7ef259eabee9a74f857fe4
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-10 12:03:34 -0500

    Compress splash image size and update link to AWS-url

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -8,7 +8,7 @@ import './Splash.css';
 const Splash = () => (
   <>
     <div className='splash-container'>
-      <img id='splash-image' src='https://images2.alphacoders.com/652/652776.jpg' alt='monopoly-background' />
+      <img id='splash-image' src='https://monopolynb.s3.amazonaws.com/splash.jpeg' alt='monopoly-background' />
       <div className='floating-message'>
         <h1>Where would you like to stay?</h1>
         <button onClick={() => alert('hey')}> Find out here </button>
