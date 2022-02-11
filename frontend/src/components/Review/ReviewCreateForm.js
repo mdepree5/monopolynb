ec2943686351ccfd2e3ec89829602158f8ac6862
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 13:13:41 -0500

    Add textarea to Review form

--- frontend/src/components/Review/ReviewCreateForm.js
+++ frontend/src/components/Review/ReviewCreateForm.js
@@ -9,7 +9,7 @@ const ReviewCreateForm = ({closeModal}) => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState(''); 
   const [rating, setRating] = useState(5);
   const [communication, setCommunication] = useState(5);
   const [checkIn, setCheckIn] = useState(5);
@@ -32,14 +32,22 @@ const ReviewCreateForm = ({closeModal}) => {
       }
     )
 
-    if(newReview || !newReview.errors) history.push(`/properties/${newReview.id}`);
+    if(newReview) alert(newReview);
+    // if(newReview || !newReview.errors) history.push(`/properties/${newReview.id}`);
 
     closeModal();
   }
 
   return (
     <Form onSub={handleSubmit} errors={errors} buttonName={'How was your stay?'} >
-      <FormInput name='Title' state={content} setState={setContent} />
+      <ul>
+        <li><label htmlFor='content'>Content</label></li>
+        <li><textarea 
+          id='content'
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          placeholder='Content'/></li>
+      </ul>
       <FormInput name='Rating' state={rating} setState={setRating} />
       <FormInput name='Communication' state={communication} setState={setCommunication} />
       <FormInput name='Check In' state={checkIn} setState={setCheckIn} />
