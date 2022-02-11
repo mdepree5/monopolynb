7b1b346bea1ef3ebda577bec53b8f476caebb826
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-10 19:41:55 -0500

    Style Splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,7 +1,5 @@
-import {Route} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import PropertyPage from '../Property/PropertyPage';
-import PropertyList from '../Property/PropertyList';
 
 import './Splash.css';
 
@@ -11,10 +9,12 @@ const Splash = () => (
       <img id='splash-image' src='https://monopolynb.s3.amazonaws.com/splash.jpeg' alt='monopoly-background' />
       <div className='floating-message'>
         <h1>Where would you like to stay?</h1>
-        <button onClick={() => alert('hey')}> Find out here </button>
+        {/* <button onClick={() => alert('hey')}> Find out here </button> */}
+        <ul id='splash-button-white'>
+          <li><NavLink to='/properties'>Where the dice land</NavLink></li>
+        </ul>
       </div>
     </div>
-    <PropertyList />
     <div className='filler-box' />
   </>
 )
