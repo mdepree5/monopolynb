65aec812f8988a5018690731323b05c6a6a8f315
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 16:43:26 -0500

    Add Date to ReviewDetail

--- frontend/src/components/Review/ReviewList.js
+++ frontend/src/components/Review/ReviewList.js
@@ -1,15 +1,22 @@
-
+// const reviewDate = date => `${date.toString().split(' ')[1]} ${date.toString().split(' ')[3]}`;
 
 // import ReviewEditModal from './ReviewEditModal';
 import ReviewDeleteButton from './ReviewDeleteButton';
 
 const ReviewDetail = ({review, belongsToUser}) => {
-  const date = review.createdAt
+  const month = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ][+review.createdAt.split('-')[1]-1];
+
+  const year = review.createdAt.split('-')[0];
+
   return (
   <ul className='review-detail'>
       <li>
         <ul className='review-nav'>
-          <li>{date}</li>
+          <li>{`${month} ${year}`}</li>
           <li>{belongsToUser && (<ReviewDeleteButton reviewId={review.id} />)}</li>
         </ul>
       </li>
