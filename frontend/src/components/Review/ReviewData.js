2bab121ac4396dd573de4b7099bf93adb1fc3416
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 18:34:13 -0500

    Refactor Meter component

--- frontend/src/components/Review/ReviewData.js
+++ frontend/src/components/Review/ReviewData.js
@@ -1,6 +1,12 @@
-import Meter from '../../context/Meter';
 import './Review.css';
 
+const Meter = ({rating}) => (
+  <div className='meter'>
+    <meter id="meter"
+    min="0" max='5'
+    value={rating} />
+  </div>
+)
 
 const ReviewData = ({reviewData, totalReviews}) => {
 
--- frontend/src/context/Meter.js
+++
@@ -1,9 +0,0 @@
-const Meter = ({rating}) => (
-  <div className='meter'>
-    <meter id="meter"
-    min="0" max='5'
-    value={rating} />
-  </div>
-)
-
-export default Meter;
