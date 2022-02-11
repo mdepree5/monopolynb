7eb337930cb0fa73347aa8b6ce74a4a98052da17
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 16:49:52 -0500

    Debug reviews api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const propertiesRouter = require('./properties.js');
-
-//* ARCHITECTURE CHOICE: Do I want to condense my routers and include bookings with user and reviews with property?
-// const bookingsRouter = require('./bookings.js');
-// const reviewsRouter = require('./reviews.js');
+const reviewsRouter = require('./reviews.js');
 
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/properties', propertiesRouter);
+router.use('/reviews', reviewsRouter);
 
 module.exports = router;
