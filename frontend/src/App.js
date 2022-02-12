3197f004019990a47d21ee01e75d2a5de12df281
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 23:31:42 -0500

    Refactor Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PageNotFound from './components/PageNotFound';
 import Footer from './components/Footer';
 // todo ——————————————————————————————————————————————————————————————————————————————————
 
+
 const App = () => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
