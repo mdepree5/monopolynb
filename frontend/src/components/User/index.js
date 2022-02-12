01e0fe2a37ba5458997219deda14d148c2eeb236
Mitch DePree <mdpree5@Mitchs-iMac.local>
2022-02-11 23:04:35 -0500

    Refactor User Page

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -17,17 +17,27 @@ const UserPage = () => {
 
   useEffect(() => {dispatch(getUserById(userId))}, [dispatch, userId]);
 
+
+  sessionUser === pageUser ? (
+    <div>
+    <div>loged in</div>
+    <li><div>My Properties</div></li>
+
+    </div>
+    ) : (
+      <div>
+      <div>not logged in</div>
+      <li><h2>{`${pageUser?.firstName}
+            ${pageUser?.lastName.slice(0, 1)}${pageUser?.lastName.slice(0, 1).endsWith('s') ? '\'' : '\'s'} Page`}
+          </h2></li>
+      </div>
+    )
+
   return (
     <div className='center-body'>
       <ul className='user-splash'>
-        <li>{sessionUser === pageUser ? (
-          <h2>{`Hello ${pageUser?.firstName}`}</h2>
-        ) : (
-          <h2>{`${pageUser?.firstName}
-            ${pageUser?.lastName.slice(0, 1)}${pageUser?.lastName.slice(0, 1).endsWith('s') ? '\'' : '\'s'} Page`}
-          </h2> )}
-        </li>
-        <li><div>My Listings</div></li>
+        <li><h2>{`Welcome Back, ${pageUser?.firstName}`}</h2></li>
+        <li><div>Your Properties</div></li>
       </ul>
       <PropertyList userId={userId} />
     </div>
