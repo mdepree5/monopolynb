f1bb9b00c1e7446f07ab2a3382f664903b935654
Mitch <mdepree315@gmail.com>
2022-04-19 11:16:37 -0400

    add image modal

--- frontend/src/components/Image/index.js
+++ frontend/src/components/Image/index.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
+import { Modal } from '../../context/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 // todo ——————————————————————————————————————————————————————————————————————————————————
 import { getImagesByPropertyId } from '../../store/image';
 import './Image.css';
 // todo ——————————————————————————————————————————————————————————————————————————————————
-
 const Image = ({propertyId}) => {
   const dispatch = useDispatch();
   const images = useSelector(state => state?.image?.listOfImages);
@@ -14,12 +14,41 @@ const Image = ({propertyId}) => {
   return images.length > 0 ? (
     <ul className='image-container'>
       {images.map(image => (
-        <li key={image?.imageURL}><img className='property-image' src={image?.imageURL} alt={`property-${image?.id}`}/></li>
+        <>
+          <ImageModal image={image} />
+          <li key={image?.imageURL}><img className='property-image' src={image?.imageURL} alt={`property-${image?.id}`}/></li>
+
+        </>
       ))}
     </ul>
   ) : (
     <div className='image-container'>No images yet!</div>
   )
 }
+
+
+const ImageModal = ({image}) => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <li 
+        key={image?.imageURL}
+        style={{cursor:'pointer'}}
+        onClick={e => setShowModal(true)}
+      >
+        <img className='property-image' src={image?.imageURL} alt={`property-${image?.id}`}/>
+      </li>
+
+      <button className={edit ? 'edit' : 'host-new-property'} onClick={e => setShowModal(true)}>{name}</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <img className='property-image' src={image?.imageURL} alt={`property-${image?.id}`}/>
+        </Modal>
+      )}
+    </>
+  );
+}
+
 
 export default Image;
