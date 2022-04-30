adfb1459ee8ea71ebfd561258573a64223ac407e
Mitch <mdepree315@gmail.com>
2022-04-29 20:40:49 -0400

    style reviews

--- frontend/src/components/Review/ReviewData.js
+++ frontend/src/components/Review/ReviewData.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { useSelector } from 'react-redux';
+// ???? ——————————————————————————————————————————————————————————————————————————————————
 import ReviewFormModal from './ReviewFormModal';
 import './Review.css';
+// ???? ——————————————————————————————————————————————————————————————————————————————————
 
-const Meter = ({rating}) => (
-  <meter id="meter" min="0" max='5' value={rating} />
-)
+const Meter = ({rating}) => <meter id="meter" min="0" max='5' value={rating} />
 
 const ReviewData = ({reviewData, totalReviews, reviews}) => {
 
@@ -23,33 +23,26 @@ const ReviewData = ({reviewData, totalReviews, reviews}) => {
 
 
   const ReviewDataLine = ({datum}) => (
-    <ul className='review-data-line' >
-      <li>{`${datum.name}:`}</li>
-      {datum.value ? (
-      <>
-        <li><Meter rating={datum.value}/></li>
-        <li>{datum.value}</li>
-      </>
-      ) : (
-        <li>no reviews yet!</li>
-      )}
-    </ul>
+    <div className='review-meter' >
+      <strong>{`${datum.name}:`}</strong>
+      {!datum.value ? <div>no reviews yet!</div> :
+        <>
+          <Meter rating={datum.value}/>
+          <strong>{datum.value}</strong>
+        </>
+      }
+    </div>
   );
 
-  const ReviewSummary = ({reviewData}) => (
-    <div className='data-header' >
-      {reviewData[0]?.value ? (
-        <h2><i className='review-star fas fa-star'/>{`${reviewData[0]?.value} · ${totalReviews} reviews`} </h2>
-      ) : (
-        <div>No reviews yet!</div>
-      )}
-    </div>
-  )
 
   return (
-    <div className='review-data-container'>
-      <ReviewSummary reviewData={reviewData}/>
-      <div className='data-line-container'>
+    <div className='review-header'>
+      
+      {!reviewData[0]?.value ? <div>No reviews yet!</div> :
+        <h2><i className='review-star fas fa-star'/>{`${reviewData[0]?.value} · ${totalReviews} reviews`} </h2>
+      }
+
+      <div className='review-meter-container'>
         {reviewData.slice(1).map(datum => (<ReviewDataLine key={datum.name} datum={datum}/>))}
       </div>
       {!belongsToUser && sessionUser && ( <ReviewFormModal reviews={reviews}/>)}
